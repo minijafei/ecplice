6b03e8c193f3107e7ba94ffe9ad8c4d5608f6da3
mini加菲 <45343809+minijafei@users.noreply.github.com>
2018-12-04 20:14:25 +0800

    update Helloword.java
    Conflicts:
    	src/myweb_01/Helloword.java

--- src/myweb_01/Helloword.java
+++ src/myweb_01/Helloword.java
@@ -4,6 +4,11 @@ public class Helloword {
 	public static void main(String[] args) {
 		System.out.println("hello word");
 		System.out.println("----------");
+		System.out.println("==========");
+		int i = 0;
+		int q = i;
+		System.out.println("==========");
+		System.out.println("==========");
 	}
 
 }
