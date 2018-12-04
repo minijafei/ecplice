05c0701f9a8f8d935a48b540731f8e775d7c70ad
mini加菲 <45343809+minijafei@users.noreply.github.com>
2018-12-04 20:30:52 +0800

    update 10

--- src/myweb_01/Bicycle.java
+++ src/myweb_01/Bicycle.java
@@ -12,6 +12,17 @@ public class Bicycle {
 		System.out.println("  \\     /   j  \\     /");
 		System.out.println("   `---'        `---'");
 		
+		System.out.println("============================");
+		
+		System.out.println("   ,--.       <__)");
+		System.out.println("   `- |________7");
+		System.out.println("      |`.      |\\");
+		System.out.println("   .--|. \\     |.\\--.");
+		System.out.println("  /   j \\ `.7__j__\\  \\");
+		System.out.println(" |    o  | (o)____o)  |");
+		System.out.println("  \\     /   j  \\     /");
+		System.out.println("   `---'        `---'");
+		
 	}
 
 }
