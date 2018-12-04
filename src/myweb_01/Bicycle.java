c236530d363706d0fbd74c224c3ca67a5a2c2031
mini加菲 <45343809+minijafei@users.noreply.github.com>
2018-12-04 13:49:42 +0800

    Add mywed

---
+++ src/myweb_01/Bicycle.java
@@ -0,0 +1,17 @@
+package D1;
+
+public class Bicycle {
+
+	public static void main(String[] args) {
+		System.out.println("   ,--.       <__)");
+		System.out.println("   `- |________7");
+		System.out.println("      |`.      |\\");
+		System.out.println("   .--|. \\     |.\\--.");
+		System.out.println("  /   j \\ `.7__j__\\  \\");
+		System.out.println(" |    o  | (o)____o)  |");
+		System.out.println("  \\     /   j  \\     /");
+		System.out.println("   `---'        `---'");
+		
+	}
+
+}
